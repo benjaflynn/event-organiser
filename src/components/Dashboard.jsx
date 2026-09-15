1d0a55e0b4be862151e4644afa2dafa6d751d694
benjaflynn <richter.ben.1997@gmail.com>
2026-09-15 12:35:23 +0100

    create login page with formik validation. integrate login page with main page.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,17 @@
 import NavBar from '../routes/NavBar';
+import LoginPage from './LoginArea.jsx';
 import { useContext } from 'react';
 import { LoginContext } from '../LoginContext';
 
 export default function Dashboard() {
 
     const { login, setLogin } = useContext(LoginContext);
+
     return (
         <div>
             <NavBar />
             {
-                login === 'loggedIn' ? <div>Welcome to your Dashboard!</div> : <div>Please log in.</div>
+                login === 'loggedIn' ? <div>Welcome to your Dashboard!</div> : <LoginPage />
             }
         </div>
     );
