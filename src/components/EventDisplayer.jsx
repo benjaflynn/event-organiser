f86ff547c14c91dc65a4cdae76e021bea343e552
benjaflynn <richter.ben.1997@gmail.com>
2026-09-21 17:33:35 +0100

    sorts events by latest or earliest

--- src/components/EventDisplayer.jsx
+++ src/components/EventDisplayer.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { EventContext } from '../EventContext';
 import EventCard from './EventCard';
 import { Row, Col, Container } from 'react-bootstrap';
+import EventSorter from './EventSorter';
 
 export default function EventDisplayer() {
 
@@ -15,13 +16,6 @@ export default function EventDisplayer() {
     };
 
     function eventEditor(values, title) {
-        console.log("the new values are: ", values);
-        console.log("the event array is: ", eventArray);
-        console.log("the clicked element title is: ", title);
-
-        let filteredIndex = eventArray.findIndex((element) => element.title === title);
-        console.log(filteredIndex);
-        
         const newEvents = eventArray.map(item => {
             if (item.title === title) {
                 return {...item, 'title': values.title, 'date': values.date, 'time': values.time, 'location': values.location, 'description': values.description}
@@ -40,6 +34,7 @@ export default function EventDisplayer() {
 
     return (
         <Container>
+            <EventSorter />
             <Col>
             {eventArray.map(event => {
                 return <Row key={event.title} id={event.title}><EventCard title={event.title} date={event.date} time={event.time} description={event.description} location={event.location} eventEditBtn={eventEditBtn} eventEditor={eventEditor} eventDeleter={eventDeleter} /></Row>})}
---
+++ src/components/EventSorter.jsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import { EventContext } from '../EventContext';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function EventSorter() {
+    const { eventArray, setEventArray } = useContext(EventContext);
+
+    function earliestFirst() {
+        const earliestEvents = [...eventArray];
+        
+        const sortByEarliestFirst = (a, b) => {
+            const aDate = new Date(`${a.date}T${a.time}`);
+            const bDate = new Date(`${b.date}T${b.time}`);
+
+            return aDate - bDate;
+        };
+
+        earliestEvents.sort(sortByEarliestFirst);
+        setEventArray(earliestEvents);
+    }
+
+    function latestFirst() {
+        const latestEvents = [...eventArray];
+        
+        const sortByLatestFirst = (a, b) => {
+            const aDate = new Date(`${a.date}T${a.time}`);
+            const bDate = new Date(`${b.date}T${b.time}`);
+
+            return bDate - aDate;
+        };
+
+        latestEvents.sort(sortByLatestFirst);
+        setEventArray(latestEvents);
+    };
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Sort
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={() => earliestFirst()}>Earliest first</Dropdown.Item>
+                <Dropdown.Item onClick={() => latestFirst()}>Latest first</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
